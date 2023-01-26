05b60270f5f158755a621e3a6f8f0e1bad845576
Mateus Santos <m.santos.azeve@gmail.com>
2023-01-26 20:37:15 -0300

    feat: navegacao para tela home

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 30}}>Tela Home</Text>
+    </View>
+  );
+};
+
+export default Home;
